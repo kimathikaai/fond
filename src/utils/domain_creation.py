4f66403eccace1ae3452f2d29ab29e06684cc23e
kimathikaai <kiprotich.kaai@gmail.com>
2024-02-18 11:51:31 -0500

    Included current class distribution strategy

--- src/utils/domain_creation.py
+++ src/utils/domain_creation.py
@@ -2,6 +2,38 @@ import math
 from typing import List
 
 
+def create_domains(
+    num_classes: int, num_linked: int, num_train_domains: int
+) -> List[List[int]]:
+    """
+    Determine and distribute domain-linked and domain-shared classes.
+    Domain-linked classes will come from the same domain, i.e., idx=0.
+    Domain-shared classes will exist in each of the remaining domains
+
+    Args:
+        num_classes: number of overall classes within all the domains
+        num_linked: number of classes that are linked to an individual domain
+        num_train_domains: number of different training domains
+    """
+    assert num_linked < num_classes
+    domain_shared = [i for i in range(num_linked, num_classes)]
+    domain_linked = [i for i in range(num_linked)]
+    print(f"Domain-shared classes: {domain_shared}")
+    print(f"Domain-linked classes: {domain_linked}")
+    # domains = [domain_shared.copy() for i in range(num_train_domains)]
+    domains = [[] for i in range(num_train_domains)]
+
+    # first domain is domain-linked only
+    domains[0] = domain_linked
+    # other domains contain domain-shared only
+    domains[1:] = [domain_shared.copy() for i in range(1, num_train_domains)]
+
+    print("domains[0]: ", domains[0])
+    print("domains[1:]: ", domains[1:])
+
+    return domains
+
+
 def create_domains_1(
     num_classes: int, num_linked: int, num_domains: int
 ) -> List[List[int]]:
@@ -24,7 +56,7 @@ def create_domains_1(
 
 
 def create_domains_2(
-    num_classes: int, num_linked_ratio: int, num_domains: int
+    num_classes: int, num_linked_ratio: float, num_domains: int
 ) -> List[List[int]]:
     """
     Args:
@@ -37,6 +69,9 @@ def create_domains_2(
 
 
 if __name__ == "__main__":
+    # Testing
+    create_domains(num_classes=10, num_linked=5, num_train_domains=3)
+
     # Testing same classes with different overlap
     domains = create_domains_1(num_classes=10, num_linked=3, num_domains=4)
     print(domains)
